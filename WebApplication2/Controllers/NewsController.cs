d2df279cc161fcaa3fe0835731ad941cd79e53e6
msmiri <msmiri@MSMIRI>
2017-02-10 14:07:35 +0100

    Agregar archivos de proyecto.

---
+++ WebApplication2/Controllers/NewsController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class NewsController : Controller
+    {
+        private NewsDBContext db = new NewsDBContext();
+
+        // GET: News
+        public ActionResult Index(string empleado, string cliente)
+        {
+
+            ViewBag.Empleado = (from r in db.News select r.Empleado).Distinct();
+            ViewBag.Cliente = (from r in db.News select r.Cliente).Distinct();
+
+            var news = from m in db.News
+                       orderby m.Empleado
+                       where m.Cliente == cliente || cliente == null || cliente == ""
+                       where m.Empleado == empleado || empleado == null || empleado == ""
+                       select m;
+
+            return View(news);
+        }
+
+        // GET: News/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            New @new = db.News.Find(id);
+            if (@new == null)
+            {
+                return HttpNotFound();
+            }
+            return View(@new);
+        }
+
+        // GET: News/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: News/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+
+        //public ActionResult Create(New FormData)
+
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "ID,Noticia,Fecha,Cliente,Empleado")] New @new)
+        {
+            Console.WriteLine(@new);
+            if (ModelState.IsValid)
+            {
+                db.News.Add(@new);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateEmployee([Bind(Include = "ID,Empleado")] New @new)
+        {
+            if (ModelState.IsValid)
+            {
+                db.News.Add(@new);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(@new);
+        }
+
+        // GET: News/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            New @new = db.News.Find(id);
+            if (@new == null)
+            {
+                return HttpNotFound();
+            }
+            return View(@new);
+        }
+
+        // POST: News/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Noticia,Fecha,Cliente,Empleado")] New @new)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(@new).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(@new);
+        }
+
+        // GET: News/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            New @new = db.News.Find(id);
+            if (@new == null)
+            {
+                return HttpNotFound();
+            }
+            return View(@new);
+        }
+
+        // POST: News/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            New @new = db.News.Find(id);
+            db.News.Remove(@new);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication2/Models/New.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Models
+{
+    public class New
+    {
+        public int ID { get; set; }
+
+        [AllowHtml]
+        public string Noticia { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Cliente { get; set; }
+        public string Empleado { get; set; }
+    }
+
+    public class NewsDBContext: DbContext
+    {
+        public DbSet<New> News { get; set; }
+    }
+}
